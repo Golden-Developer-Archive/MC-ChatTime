17a7338bd680b17a389bb636740a025e080fa8eb
_Coho04_ <73216313+Coho04@users.noreply.github.com>
2021-11-12 12:59:14 +0100

    Initial commit

---
+++ src/main/java/de/_coho04_/chattime/Event.java
@@ -0,0 +1,19 @@
+package de._coho04_.chattime;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Event implements Listener {
+
+    @EventHandler
+    public void onMessage(AsyncPlayerChatEvent e) {
+        Player p = (Player)e.getPlayer();
+        DateTimeFormatter dtf5 = DateTimeFormatter.ofPattern("HH:mm:ss");
+        e.setFormat("§6[§a" + dtf5.format(LocalDateTime.now()) + "§6] §r" + p.getDisplayName() + " » " + e.getMessage());
+    }
+}
---
+++ src/main/java/de/_coho04_/chattime/main.java
@@ -0,0 +1,18 @@
+package de._coho04_.chattime;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage("§a[§6ChatTime§a]§a now Online!");
+        Bukkit.getPluginManager().registerEvents(new Event(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage("§a[§6ChatTime§a]§c now Offline!");
+    }
+}
